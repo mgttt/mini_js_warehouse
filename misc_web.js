827436622203bbc4aa9c97c41662de5b64a5986a
WANJO CHAN <wanjochan@users.noreply.github.com>
2018-03-13 22:54:57 +0800

    Update misc_web.js
    function getQueryStr(){return location.search.replace(/^\?/g,"")+'&'+location.hash.replace(/^#/g,"")}
    function getQueryVar(k){if(!this._qva){var _qva={};getQueryStr().replace(/([^?=&]+)(=([^&]*))?/g,function($0,$1,$2,$3){_qva[$1]=$3});this._qva=_qva};return k?this._qva[k]:this._qva}

--- misc_web.js
+++ misc_web.js
@@ -6,9 +6,12 @@ function copy_o2o(o1,o2,o3){var o=o3||o2;for(var k in o){o1[k]=o2[k]}return o1}
 //function getQueryStr(){return (new String(location.search)).replace(/^\?/g,"")}
 //function getQueryVar(sVar){ _qva=this._qva=getQueryStringA(); return _qva[sVar]}
 
+//function getQueryStr(){return location.search.replace(/^\?/g,"")+'&'+location.hash.replace(/^#/g,"")}
+//function getQueryStringA(k){if(!this._qva){var _qva={};getQueryStr().replace(/([^?=&]+)(=([^&]*))?/g,function($0,$1,$2,$3){_qva[$1]=$3});this._qva=_qva};return k?this._qva[k]:this._qva}
+//function getQueryVar(k){return getQueryStringA(k)}
+
 function getQueryStr(){return location.search.replace(/^\?/g,"")+'&'+location.hash.replace(/^#/g,"")}
-function getQueryStringA(k){if(!this._qva){var _qva={};getQueryStr().replace(/([^?=&]+)(=([^&]*))?/g,function($0,$1,$2,$3){_qva[$1]=$3});this._qva=_qva};return k?this._qva[k]:this._qva}
-function getQueryVar(k){return getQueryStringA(k)}
+function getQueryVar(k){if(!this._qva){var _qva={};getQueryStr().replace(/([^?=&]+)(=([^&]*))?/g,function($0,$1,$2,$3){_qva[$1]=$3});this._qva=_qva};return k?this._qva[k]:this._qva}
 
 function getSID(){ return getQueryVar('_s'); }
 function ajaxqq(api,param){
