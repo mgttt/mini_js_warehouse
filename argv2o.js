7bd13014fd05686f31ba1407f212bd345b935bd3
WANJO CHAN <wanjochan@users.noreply.github.com>
2018-03-21 17:28:48 +0800

    Update argv2o.js
    shorter and bitter of ES6 version

--- argv2o.js
+++ argv2o.js
@@ -1,5 +1,10 @@
+//ES5
 function argv2o(argv){
 	var m,mm,rt={};
 	for(k in argv)(m=(rt[""+k]=argv[k]).match(/^--?([a-zA-Z0-9-_]*)=(.*)/))&&(rt[m[1]]=(mm=m[2].match(/^".*"$/))?mm[1]:m[2]);
 	return rt;
 }
+function argv2o(argv,m,mm){var rt={};for(k in argv)(m=(rt[""+k]=argv[k]).match(/^(\/|--?)([a-zA-Z0-9-_]*)="?(.*)"?$/))&&(rt[m[2]]=m[3]);return rt}
+
+//ES6
+const argv2o=o=>o.reduce((r,e)=>(m=e.match(/^(\/|--?)([a-zA-Z0-9-_]*)="?(.*)"?$/))&&(r[m[2]]=m[3])&&r||r,{});
