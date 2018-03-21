88a61ce3a88338930c142c4f7063fb7fe421b8c1
WANJO CHAN <wanjochan@users.noreply.github.com>
2018-03-21 11:55:39 +0800

    Create fix_stdin_nwjs.js

---
+++ fix_stdin_nwjs.js
@@ -0,0 +1,23 @@
+//NOTE: demo fix stdin for nwjs... can fix at logic.js if needed.
+//@ref https://github.com/cotejp/nwjs-j5-fix/blob/master/nwjs-j5-fix.js
+var obj = {
+	fix: function() {
+		if (!process.versions.nw || parseFloat(process.versions.nw) < 0.13) {
+			throw "This module is only relevant when run under NW.js v0.13 or higher."
+		}
+		var Readable = nw.require("stream").Readable;
+		var util = nw.require("util");
+		function MyStream(opt) {
+			Readable.call(this, opt);
+		}
+		util.inherits(MyStream, Readable);
+		MyStream.prototype._read = function() {};
+		process.__defineGetter__("stdin", function() {
+			if (process.__stdin) return process.__stdin;
+			process.__stdin = new MyStream();
+			return process.__stdin;
+		});
+	}
+};
+obj.abracadabra = obj.fix;
+module.exports = obj;
