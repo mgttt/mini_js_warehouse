c950e9c6bd2bab59bf34199e093ed6f954d27cbf
WANJO CHAN <wanjochan@users.noreply.github.com>
2018-12-30 22:28:49 +0800

    Update copy_o2o.js

--- copy_o2o.js
+++ copy_o2o.js
@@ -1,1 +1,4 @@
-function copy_o2o(o1,o2,o3){var o=o3||o2;for(var k in o){o1[k]=o2[k]}return o1}
+//function copy_o2o(o1,o2,o3){var o=o3||o2;for(var k in o){o1[k]=o2[k]}return o1}
+//2018.12.30
+function copy_o2o(o1,o2,o3){for(var k in (o3||o2)){o1[k]=o2[k]}return o1}
+
