3809c9371503c86c7d9b754bf7ae7f182a397012
WANJO CHAN <wanjochan@users.noreply.github.com>
2019-01-09 18:07:59 +0800

    tinyfsm.js

--- tiny_fsm_eg.js
+++ tiny_fsm_eg.js
@@ -30,3 +30,44 @@ RoundEnd          .OK => RoundStart      // let do next round
 .done(rst=>{
 	console.log('fsm.rst=',rst);
 });
+
+tinyfsm.js
+```
+module.exports = (lgc,fsm_data,step_start)=>{
+	//parse fsm from str to obj:
+	var fsm_o = (typeof fsm_data=='object') ? fsm_data : fsm_data.split(/[\n\r]+/).reduce(
+		(r,e)=>(m=e.replace(/\s/g,'').match(/^(\w*)(\.(\w*)=>(\w*))?/))&&(r[m[1]]=(r[m[1]]||{}),r[m[1]][m[3]]=m[4],r),{}
+	);
+	//NOTES: may add check fsm_o for the broken links
+
+	if(!step_start){ for(var nm in fsm_o){ if(nm){step_start=nm; break}} }
+
+	//tiny fsm exec engine
+	var fsm_func = (step_name,prev_nm,prev_sts,prev_rst) =>
+		lgc.call(step_name,prev_nm,prev_sts,prev_rst).then((rst,STS,step_next)=>(
+			STS = (rst||{}).STS,
+			step_next = (fsm_o[step_name]||{})[STS],
+			step_next ? fsm_func(step_next,step_name,STS,rst) : rst
+		));
+
+	return fsm_func(step_start);
+};
+```
+calling example:
+```
+const argv2o=a=>a.reduce((r,e)=>(m=e.match(/^(\/|--?)([a-zA-Z0-9-_]*)="?(.*)"?$/))&&(r[m[2]]=m[3])&&r||r,{});
+require('./tinyfsm')(require('./LgcHW')(argv2o(process.argv)),`
+RoundStart        .OK => MaintainSession // keep login
+MaintainSession   .OK => CheckData       // collect and analyze data
+CheckData         .OK => RoundEndSleep   // sleep & next if no action needed
+CheckData         .ST => PerformStrategy // jump calc if action needed
+PerformStrategy   .OK => RoundEndSleep   // skip the sleep
+RoundEndSleep     .OK => RoundEnd        // might sleep few seconds
+RoundEnd          .OK => RoundStart      // let do next round
+`)
+	.fail(err=>err)
+	.done(rst=>{
+		console.log('fsm.rst=',rst);
+	});
+
+```
